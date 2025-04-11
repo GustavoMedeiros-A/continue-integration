698cbf1861642e7890e1e7fcc4c7003dda38dbc3
GustavoMedeiros-A <gustavomedeiros15@outlook.com.br>
2025-04-11 13:44:30 -0300

    feat: implement and create test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GustavoMedeiros-A/continue-integration
+
+go 1.23.6
---
+++ math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSuM(t *testing.T) {
+	total := Sum(15, 15)
+	if total != 30 {
+		t.Errorf("Wrong result! Result %d. Expected: %d", total, 30)
+	}
+}
